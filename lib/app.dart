2469c41ae6e5d87cc602b08ac3b6fdafa762f919
Ahmet Kahrimanoglu <ahmet.kahrimanoglu@gmail.com>
2024-07-18 09:56:47 +0300

    Proje için klasör yapısı oluşturuldu

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BTK Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Center(
+        child: Text(
+          'Flutter Demo Home Page',
+        ),
+      ),
+    );
+  }
+}
