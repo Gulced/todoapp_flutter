7a68d57abcfee40134aff5d79075079a3b875aed
Ahmet Kahrimanoglu <ahmet.kahrimanoglu@gmail.com>
2024-07-18 15:35:46 +0300

    TokenRepoistory sınıfımıza Either type’ı eklendi

--- lib/domain/token_repository.dart
+++ lib/domain/token_repository.dart
@@ -1,10 +1,16 @@
+import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
+import '/core/core.dart';
 import '/data/data.dart';
 
 abstract class ITokenRepository {
-  Future<AuthResponse> registerUser({required RegisterRequest request});
-  Future<AuthResponse> loginUser({required LoginRequest request});
+  Future<Either<LoginFailure, AuthResponse>> registerUser({
+    required RegisterRequest request,
+  });
+  Future<Either<LoginFailure, AuthResponse>> loginUser({
+    required LoginRequest request,
+  });
 }
 
 @LazySingleton(as: ITokenRepository)
@@ -17,11 +23,64 @@ class TokenRepository implements ITokenRepository {
 
   ///
   @override
-  Future<AuthResponse> registerUser({required RegisterRequest request}) =>
-      tokenClient.registerUser(request);
+  Future<Either<LoginFailure, AuthResponse>> loginUser({
+    required LoginRequest request,
+  }) async {
+    /// [Giriş Yap]
+    try {
+      final response = await tokenClient.loginUser(request);
 
-  ///
+      /// Kullanıcı girişi [Başarısız] ise
+      if (!response.success!) {
+        ///
+        return const Left(
+          LoginFailure(
+            message: 'Giriş Başarısız',
+          ),
+        );
+      }
+
+      /// [Başarılı ise]
+      return Right(response);
+
+      ///
+    } catch (e) {
+      ///
+      return Left(
+        LoginFailure(
+          message: '$e',
+        ),
+      );
+    }
+  }
+
+  /// [Kaydol Metodu]
   @override
-  Future<AuthResponse> loginUser({required LoginRequest request}) =>
-      tokenClient.loginUser(request);
+  Future<Either<LoginFailure, AuthResponse>> registerUser({
+    required RegisterRequest request,
+  }) async {
+    try {
+      ///
+      final response = await tokenClient.registerUser(request);
+
+      /// [Başarısız ise]
+      if (!response.success!) {
+        return const Left(
+          LoginFailure(
+            message: 'Kayıt İşlemi Başarısız',
+          ),
+        );
+      }
+
+      ///
+      return Right(response);
+    } catch (e) {
+      ///
+      return Left(
+        LoginFailure(
+          message: '$e',
+        ),
+      );
+    }
+  }
 }
