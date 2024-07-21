1a951d2e0414cd4e04825a5cffc5316a05c0774b
Ahmet Kahrimanoglu <ahmet.kahrimanoglu@gmail.com>
2024-07-21 22:43:19 +0300

    MaterialApp’te bulunan Lokalizasyon Parametreleri Aktif edildi

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '/core/core.dart';
 import '/login/login.dart';
@@ -15,11 +16,22 @@ class App extends StatelessWidget {
 
       ///
       child: MaterialApp.router(
+        title: 'BTK Todo',
+        debugShowCheckedModeBanner: false,
+
+        /// [Router]
         routerConfig: router,
 
-        ///
+        /// [Theme]
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
+
+        /// [L10n]
+        /// Delegate: Temsilci Listesi
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+
+        /// Desteklenen Diller
+        supportedLocales: AppLocalizations.supportedLocales,
       ),
     );
   }
